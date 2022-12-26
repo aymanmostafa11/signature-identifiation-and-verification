5ac3a939723a8983f2aeb02d7700d9c660f602d7
aymanmostafa11 <ayman20191700162@cis.asu.edu.eg>
2022-12-26 07:24:54 +0200

    Edited running from command

--- main.py
+++ main.py
@@ -7,23 +7,33 @@ from models.Pipelines import CNN_encoder_Pipeline
 from PIL import Image
 import argparse
 
+root_dir = os.path.abspath(".")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", default="test", choices=["train", "test"],
-                        help="Choose the mode to run the script")
-    parser.add_argument("--img_path", required=True,
-                        help="The path of the image to be inferred")
+    parser.add_argument("mode", choices=["infer", "evaluate"], help="Choose the mode to run the"
+                                                                                   "pipeline on")
+    parser.add_argument("-p", "--path", help="the path to infer/evaluate"
+                                             "if 'train' or 'test' is provided "
+                                             "the process is done on the existing data", required=True)
 
-    args = vars(parser.parse_args())
+    args = parser.parse_args()
 
-    if args['mode'] == 'test':
-        try:
-            img = DataManager.read_image(args["img_path"])
-        except FileNotFoundError as e:
-            print(f"Img not found at given path: {args['img_path']}")
-            exit()
+    if args.mode == "infer":
+        path = args.path
+        path = os.path.join(root_dir, args.path)
+        if os.path.isdir(path):  # batch mode
+            pass  # Run pipeline on batch
+        else:
+            try:
+                img = DataManager.read_image(path)
+            except FileNotFoundError as e:
+                print(f"Img not found at given path: {args['img_path']}")
+                exit()
 
-        pipeline = CNN_encoder_Pipeline()
-        pipeline.run_single(img)
+            pipeline = CNN_encoder_Pipeline()
+            pipeline.run_single(img)
 
+    elif args.mode == "evaluate":
+        pass
 
